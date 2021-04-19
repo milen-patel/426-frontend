06f7941865c469d58c5d76ad680f873ad8d617c3
Milen <milen@live.unc.edu>
2021-04-19 16:36:34 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
 		  Welcome to my final project!
         </p>
+	  <p> Test </p>
         <a
           className="App-link"
           href="https://reactjs.org"
