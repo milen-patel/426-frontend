2d668d3c4eb4a477b1a99dfb657cd1e42ff7b13b
Milen <milen@live.unc.edu>
2021-05-07 11:36:51 -0400

    Added global stats to homepage

--- src/App.js
+++ src/App.js
@@ -8,36 +8,17 @@ import Login from "./components/Login";
 import AccountInfoView from "./components/AccountInfoView";
 import Dashboard from "./components/Dashboard";
 import Leaderboard from "./components/Leaderboard";
-import { useHistory } from "react-router";
+import Homepage from "./components/Homepage";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./app.css";
 //import Jumbotron from 'react-bootstrap/Jumbotron'
 
 
 function App() {
-  // Hook for changing browser destination
-  let history = useHistory();
-
     return (
       <Switch>
         <Route exact path="/Earth3-Frontend/">
-          <div class = 'container'>
-            <div class ='row-md-6'>
-              <div class ='col-md-6'>
-                <div class = 'card'>
-                  <form>
-                    <h1 class="display-3">Welcome to the Game</h1>
-                    <button  onClick={() => {
-                      history.push("/Earth3-Frontend/login")
-                    }}>Move to Login</button>
-                    <button  onClick={() => {
-                      history.push("/Earth3-Frontend/register")
-                    }}>Move to Register</button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Homepage /> 
         </Route>
         <Route path="/Earth3-Frontend/login">
           <Login />
---
+++ src/components/Homepage.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: null,
+      numPlayers: 0,
+      numProperties: 0,
+      totalEarnings: 0,
+    };
+  }
+
+  async componentDidMount() {
+    // Call the backend to get user account information
+    const res = await axios({
+        method: "get",
+        url: "https://backend-426.herokuapp.com/api/stats",
+    });
+    if (res.status !== 200) {
+        return;
+    }
+
+    this.setState(() => ({
+        numPlayers: res.data.numUsers,
+        numProperties: res.data.numProperties,
+        totalEarnings: res.data.totalEarnings,
+    }));
+  }
+
+  // Helper for turning big int into dollar readable format
+  numberWithCommas(x) {
+    if (!x) {
+      return "";
+    }
+    x = parseFloat(x.toFixed(2));
+    return "$" + x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return this.state.redirect;
+    }
+    return (
+      <div class="container">
+        <div class="row-md-6">
+          <div class="col-md-6">
+            <div class="card">
+              <h1>Active Players: {this.state.numPlayers}</h1>
+              <h1>Total Properties: {this.state.numProperties}</h1>
+              <h1>Total Revenue Generated: {this.numberWithCommas(this.state.totalEarnings)}</h1>
+            </div>
+            <div class="card">
+              <form>
+                <h1 class="display-3">Welcome to the Game</h1>
+                <button
+                  onClick={() => {
+                    this.setState(() => ({
+                      redirect: (
+                        <Redirect to="/Earth3-Frontend/login"></Redirect>
+                      ),
+                    }));
+                  }}
+                >
+                  Move to Login
+                </button>
+                <button
+                  onClick={() => {
+                    this.setState(() => ({
+                      redirect: (
+                        <Redirect to="/Earth3-Frontend/register"></Redirect>
+                      ),
+                    }));
+                  }}
+                >
+                  Move to Register
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Homepage;
